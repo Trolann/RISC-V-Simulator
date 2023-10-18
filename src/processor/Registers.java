6ff2982d8c1256f1a81deccf8a4f37af3a9dca69
Trolann <trevor@mathisen.dev>
2023-10-18 20:36:41 +0100

    registers class created

--- src/processor/Registers.java
+++ src/processor/Registers.java
@@ -1,2 +1,35 @@
 package processor;
 
+import java.util.HashMap;
+
+public class Registers {
+
+    private final HashMap<String, String> registerMap;
+
+    public Registers() {
+        this.registerMap = new HashMap<>();
+
+        // Initialize general-purpose registers x1 to x31
+        for (int i = 0; i <= 31; i++) {
+            registerMap.put("x" + i, "00000000000000000000000000000000");
+        }
+        registerMap.put("pc", "00000000000000000000000000000000");
+    }
+
+    public void setRegisterValue(String registerKey, String value) {
+        // Only set the value if the key exists (no new keys allowed)
+        if (registerMap.containsKey(registerKey) && registerKey != "x0" && value.length() == 32) {
+            registerMap.put(registerKey, value);
+        }
+    }
+
+
+    public String getRegisterValue(String registerKey) {
+        return registerMap.getOrDefault(registerKey, null);
+    }
+
+    @Override
+    public String toString() {
+        return registerMap.toString();
+    }
+
