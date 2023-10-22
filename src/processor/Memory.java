6f70a078015e19f1a06f1eaebee2b1d586387eb4
sharanyaudupa <sharanyaudupa@localhost>
2023-10-22 09:42:36 -0700

    memory class created/outline

---
+++ src/processor/Memory.java
@@ -0,0 +1,25 @@
+package processor;
+
+import java.util.HashMap;
+
+public class Memory {
+
+  private HashMap<String, String> memoryMap;
+  
+  public Memory() {
+    memoryMap = new HashMap<>(); 
+  }
+
+  public void setMemoryValue(String address, String value) {
+    memoryMap.put(address, value); 
+  }
+
+  public String getMemoryValue(String address) {
+    if(memoryMap.containsKey(address)) {
+      return memoryMap.get(address);
+    } else {
+      return "00000000000000000000000000000000"; // default to 0
+    }
+  }
+
+}
