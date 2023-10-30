70f44e66a564ca5d6164a3a99abe66cd4a7fdce5
Arjay <142987195+ArjayReyes@users.noreply.github.com>
2023-10-30 08:52:52 -0700

    Update Utility.java
    Added loadData method from Loader to Utility

--- src/processor/Utility.java
+++ src/processor/Utility.java
@@ -1,5 +1,10 @@
 package processor;
 
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Utility {
     /**
      * Adds the given integer value to a 32-bit binary string and returns
@@ -29,6 +34,35 @@ public class Utility {
         }
 
         return result;
+    }
+
+    public static void loadData(String filePath, Memory memory) {
+        try (DataInputStream dataInputStream = new DataInputStream(new FileInputStream(filePath))) {
+            int count = 0;
+            String memoryAddress = "00000000000000000000000000000000";
+            String instruction = "";
+
+            // Read data from the file
+            while (dataInputStream.available() > 0) {
+                if (count < 4) {
+                    String line = dataInputStream.readLine();
+                    instruction = line + instruction;
+                    count++;
+                }
+                if (count == 4) {
+                    //System.out.println(instruction);  // use to check instruction read/printed correctly
+                    memory.setMemoryValue(memoryAddress, instruction);
+                    memoryAddress = StringCrement(memoryAddress, 1);
+                    instruction = dataInputStream.readLine();
+                    count = 1;
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + filePath);
+        } catch (IOException e) {
+            System.out.println("Error reading the file: " + e.getMessage());
+        }
     }
 
     public static void main(String[] args) {
