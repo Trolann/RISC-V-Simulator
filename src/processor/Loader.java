dd1f6c815e67697dec3ee5dd70e1030058d12257
sharanya2003 <73152852+sharanya2003@users.noreply.github.com>
2023-11-19 18:06:46 -0800

    Loader class created

---
+++ src/processor/Loader.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+public class Loader {
+
+  private Memory memory;
+
+  public Loader(Memory memory) {
+    this.memory = memory;
+  }
+
+  public void loadInstructions(String filename) throws IOException {
+
+    FileInputStream fis = new FileInputStream(filename);
+    DataInputStream dis = new DataInputStream(fis);
+
+    String address = "00000000000000000000000000000000";
+
+    while(dis.available() > 0) {
+     
+      String instruction = "";
+
+      for(int i=0; i<4; i++) {
+        String line = dis.readLine();
+        instruction = line + instruction; 
+      }
+
+      memory.setMemoryValue(address, instruction);
+      address = Utility.StringCrement(address, 1);
+    }
+
+    dis.close();
+    fis.close();
+  }
+}
