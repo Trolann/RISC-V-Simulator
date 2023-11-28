ff78ba4519d3859d65b0b7825cc9491eef6c9669
sitang2 <124098507+sitang2@users.noreply.github.com>
2023-11-27 17:56:43 -0800

    Update

--- src/processor/Instructions.java
+++ src/processor/Instructions.java
@@ -3,66 +3,6 @@ package processor;
 import java.util.HashSet;
 
 public class Instructions {
-    public String LUI(HashSet<String> asmInstruction, int immediate, int rd) {
-        String instruction = "LUI " + "x" + rd + ", " + immediate;
-        asmInstruction.add(instruction);
-        return instruction;
-    }
-    
-    public String AUIPC(HashSet<String> asmInstruction, int immediate, int rd) {
-        String instruction = "AUIPC  " + "x" + rd + ", " + immediate;
-        asmInstruction.add(instruction);
-        return instruction;
-    }
-    
-    public String JAL(HashSet<String> asmInstruction,  int immediate, int rd) {
-        String instruction = "JAL " + "x" + rd + ", " + immediate;
-        asmInstruction.add(instruction);
-        return instruction;
-    }
-    
-    public String JALR(HashSet<String> asmInstruction, int immediate, int rs1, int rd) {
-        String instruction = "JALR " + "x" + rd + ", " + immediate + "(x" + rs1 + ")";
-        asmInstruction.add(instruction);
-        return instruction;
-    }
-    
-    public String BEQ(HashSet<String> asmInstruction, int rs1, int rs2, int immediate) {
-        String instruction = "BEQ " + "x" + rs1 + ", x" + rs2 + ", " + immediate;
-        asmInstruction.add(instruction);
-        return instruction;
-    }
-    
-    public String BNE(HashSet<String> asmInstruction, int rs1, int rs2, int immediate) {
-        String instruction = "BNE " + "x" + rs1 + ", x" + rs2 + ", " + immediate;
-        asmInstruction.add(instruction);
-        return instruction;
-    }
-    
-    public String BLT(HashSet<String> asmInstruction, int rs1, int rs2, int immediate) {
-        String instruction = "BEQ " + "x" + rs1 + ", x" + rs2 + ", " + immediate;
-        asmInstruction.add(instruction);
-        return instruction;
-    }
-    
-    public String BGE(HashSet<String> asmInstruction, int rs1, int rs2, int immediate) {
-        String instruction = "BEQ " + "x" + rs1 + ", x" + rs2 + ", " + immediate;
-        asmInstruction.add(instruction);
-        return instruction;
-    }
-    
-    public String BLTU(HashSet<String> asmInstruction, int rs1, int rs2, int immediate) {
-        String instruction = "BLTU " + "x" + rs1 + ", x" + rs2 + ", " + immediate;
-        asmInstruction.add(instruction);
-        return instruction;
-    }
-    
-    public String BGEU(HashSet<String> asmInstruction, int rs1, int rs2, int immediate) {
-        String instruction = "BGEU " + "x" + rs1 + ", x" + rs2 + ", " + immediate;
-        asmInstruction.add(instruction);
-        return instruction;
-    }
-
     private Memory memory;
     private Registers registers;
 
@@ -70,6 +10,169 @@ public class Instructions {
         this.memory = memory;
         this.registers = registers;
     }
+
+    	public HashMap<String, Object> executeInstruction(String opcode, HashMap<String, String> instructionComponents) {
+		HashMap<String, Object> result = new HashMap<>();
+
+		switch (opcode) {
+		case "LUI":
+			result.put("asmInstruction", LUI(instructionComponents));
+			break;
+		case "AUIPC":
+			result.put("asmInstruction", AUIPC(instructionComponents));
+			break;
+		case "JAL":
+			result.put("asmInstruction", JAL(instructionComponents));
+			result.put("newPC", calculateNewPC(instructionComponents));
+			break;
+		case "JALR":
+			result.put("asmInstruction", JALR(instructionComponents));
+			result.put("newPC", calculateNewPC(instructionComponents));
+			break;
+		case "BEQ":
+			result.put("asmInstruction", BEQ(instructionComponents));
+			result.put("newPC", calculateBranchPC(instructionComponents));
+			break;
+		// Add cases for other instructions
+
+		default:
+			throw new UnsupportedOperationException("Unsupported opcode: " + opcode);
+		}
+
+		return result;
+	}
+
+	private int calculateBranchPC(HashMap<String, String> instructionComponents) {
+		// Calculate the new program counter for branch instructions
+		int currentPC = registers.getProgramCounter();
+		// Perform the calculation based on the branch instruction and update the PC
+		// Replace with the actual calculation based on branch conditions
+		int newPC = currentPC + 4; // Placeholder, replace with actual branch calculation
+		registers.setProgramCounter(newPC);
+		return newPC;
+	}
+
+	private int calculateNewPC(HashMap<String, String> instructionComponents) {
+		// Calculate the new program counter based on the instruction
+		int currentPC = registers.getProgramCounter();
+		// Perform the calculation based on the instruction and update the PC
+		int newPC = currentPC + 4; // Assuming a simple increment for the next instruction
+		registers.setProgramCounter(newPC);
+		return newPC;
+	}
+
+	private String LUI(HashMap<String, String> instructionComponents) {
+		// Extract components from the HashMap
+		String rd = instructionComponents.get("rd");
+		String imm = instructionComponents.get("imm");
+
+		// Convert immediate value from binary string to integer
+		int immediate = Integer.parseInt(imm, 2);
+
+		// Left shift immediate by 12 bits to get the result
+		int result = immediate << 12;
+
+		// Convert result back to binary string representation
+		String resultBinary = Integer.toBinaryString(result);
+
+		// Ensure resultBinary is 32-bit length
+		while (resultBinary.length() < 32) {
+			resultBinary = "0" + resultBinary;
+		}
+
+		// Update rd register value
+		registers.setRegisterValue(rd, resultBinary);
+
+		return "LUI assembly instruction";
+	}
+
+	private String AUIPC(HashMap<String, String> instructionComponents) {
+		// Extract components from the HashMap
+		String rd = instructionComponents.get("rd");
+		String imm = instructionComponents.get("imm");
+
+		// Convert immediate value from binary string to integer
+		int immediate = Integer.parseInt(imm, 2);
+
+		// Add immediate to the current PC value
+		int result = registers.getProgramCounter() + immediate;
+
+		// Convert result back to binary string representation
+		String resultBinary = Integer.toBinaryString(result);
+
+		// Ensure resultBinary is 32-bit length
+		while (resultBinary.length() < 32) {
+			resultBinary = "0" + resultBinary;
+		}
+
+		// Update rd register value
+		registers.setRegisterValue(rd, resultBinary);
+
+		return "AUIPC assembly instruction";
+	}
+
+	private String JAL(HashMap<String, String> instructionComponents) {
+		// Extract components from the HashMap
+		String rd = instructionComponents.get("rd");
+		String imm = instructionComponents.get("imm");
+
+		// Convert immediate value from binary string to integer
+		int immediate = Integer.parseInt(imm, 2);
+
+		// Calculate the new program counter
+		int newPC = registers.getProgramCounter() + immediate;
+
+		// Update rd register value with the old PC + 4
+		registers.setRegisterValue(rd, Integer.toBinaryString(registers.getProgramCounter() + 4));
+
+		return "JAL assembly instruction";
+	}
+
+	private String JALR(HashMap<String, String> instructionComponents) {
+		// Extract components from the HashMap
+		String rd = instructionComponents.get("rd");
+		String rs1 = instructionComponents.get("rs1");
+		String imm = instructionComponents.get("imm");
+
+		// Get values from registers
+		String valueRs1 = registers.getRegisterValue(rs1);
+		int valueIntRs1 = Integer.parseInt(valueRs1, 2);
+
+		// Convert immediate value from binary string to integer
+		int immediate = Integer.parseInt(imm, 2);
+
+		// Calculate the new program counter
+		int newPC = (valueIntRs1 + immediate) & ~1;
+
+		// Update rd register value with the old PC + 4
+		registers.setRegisterValue(rd, Integer.toBinaryString(registers.getProgramCounter() + 4));
+
+		return "JALR assembly instruction";
+	}
+
+	private String BEQ(HashMap<String, String> instructionComponents) {
+		// Extract components from the HashMap
+		String rs1 = instructionComponents.get("rs1");
+		String rs2 = instructionComponents.get("rs2");
+		String imm = instructionComponents.get("imm");
+
+		// Get values from registers
+		String valueRs1 = registers.getRegisterValue(rs1);
+		String valueRs2 = registers.getRegisterValue(rs2);
+
+		// Convert immediate value from binary string to integer
+		int immediate = Integer.parseInt(imm, 2);
+
+		// Check if the values in rs1 and rs2 are equal
+		if (valueRs1.equals(valueRs2)) {
+			// Calculate the new program counter
+			int newPC = registers.getProgramCounter() + immediate;
+
+			return "BEQ assembly instruction";
+		} else {
+			return "Branch not taken";
+		}
+	}
     
     
     public void addi(HashMap<String, String> instructionComponents) {
