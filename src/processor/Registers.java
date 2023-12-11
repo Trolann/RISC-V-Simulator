f6550872c116b2ef7459c296f4e743e09ccbbffa
Trolann <trevor@mathisen.dev>
2023-12-11 09:39:33 -0800

    updated aliases for later printing

--- src/processor/Registers.java
+++ src/processor/Registers.java
@@ -22,7 +22,40 @@ public class Registers {
     }
 
     private void initializeRegistersAndAliases() {
-        String[] aliases = {"zero", "ra", "sp", "gp", "tp", "t0", "t1", "t2", "s0/fp", "s1", "a0", "a1", "a2", "a3", "a4", "a5", "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"};
+        String[] aliases = {
+                "zero", // x0
+                "ra", // x1
+                "sp", // x2
+                "gp", // x3
+                "tp", // x4
+                "t0", // x5
+                "t1", // x6
+                "t2", // x7
+                "s0/fp", // x8
+                "s1", // x9
+                "a0", // x10
+                "a1", // x11
+                "a2", // x12
+                "a3", // x13
+                "a4", // x14
+                "a5", // x15
+                "a6", // x16
+                "a7", // x17
+                "s2", // x18
+                "s3", // x19
+                "s4", // x20
+                "s5", // x21
+                "s6", // x22
+                "s7", // x23
+                "s8", // x24
+                "s9", // x25
+                "s10", // x26
+                "s11", // x27
+                "t3", // x28
+                "t4", // x29
+                "t5", // x30
+                "t6" // x31
+        };
         for (int i = 0; i <= 31; i++) {
             String registerKey = "x" + i;
             registerMap.put(registerKey, Utility.ALLZEROS);
