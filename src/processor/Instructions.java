371f5cb3e3fa006861bae7328bd11bc1ea615704
Trolann <trevor@mathisen.dev>
2023-12-11 11:27:35 -0800

    change ADDI to use new leftpad

--- src/processor/Instructions.java
+++ src/processor/Instructions.java
@@ -271,10 +271,6 @@ public class Instructions {
 		int result = valueIntRs1 + immediate;
 		System.out.println("ADDI DEBUG: Adding " + valueIntRs1 + " and " + immediate + " to get " + result);
 		// Convert result to 32-bit binary string
-		// String resultBinary = Utility.leftPad("0" + Integer.toBinaryString(result));
-		// if (result < 0 ) {
-		// 	resultBinary = Utility.leftPad("1" + Integer.toBinaryString(result));
-		// }
 		String resultBinary = Utility.leftPad(Integer.toBinaryString(result));
 		// Update rd register value
 		registers.setRegisterValue(rd, resultBinary);
